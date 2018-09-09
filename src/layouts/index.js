be1f062bb7744cf4de2c88d751337cc03e8e779e
ReedSun <superreedsun@gmail.com>
2018-09-09 22:12:03 +0800

    remove umi-cli native page content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,18 @@
-import styles from './index.css';
+/*
+ * File: index.js
+ * Project: reedsun-homepage
+ * File Created: Sunday, 9th September 2018 7:40:34 pm
+ * Author: ReedSun (sunhongzhao@foxmail.com)
+ * -----
+ * Last Modified: Sunday, 9th September 2018 10:10:59 pm
+ * Modified By: ReedSun (sunhongzhao@foxmail.com>)
+ * -----
+ * Copyright 2018 - 2018, Shanjing
+ */
+import React from 'react';
 
-function BasicLayout(props) {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      { props.children }
-    </div>
-  );
+function BasicLayout({ children }) {
+  return <div>{children}</div>;
 }
 
 export default BasicLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
-import styles from './index.css';
+/*
+ * File: index.js
+ * Project: reedsun-homepage
+ * File Created: Sunday, 9th September 2018 7:40:34 pm
+ * Author: ReedSun (sunhongzhao@foxmail.com)
+ * -----
+ * Last Modified: Sunday, 9th September 2018 10:11:04 pm
+ * Modified By: ReedSun (sunhongzhao@foxmail.com>)
+ * -----
+ * Copyright 2018 - 2018, Shanjing
+ */
+import React from 'react';
 
 export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+  return <div>首页</div>;
 }
