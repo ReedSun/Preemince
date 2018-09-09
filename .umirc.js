13c26b3cf39e578735207d959ad1d71251104e46
ReedSun <superreedsun@gmail.com>
2018-09-09 22:13:10 +0800

    set page name to 'ReedSun'

--- .umirc.js
+++ .umirc.js
@@ -1,19 +1,32 @@
-
+/*
+ * File: .umirc.js
+ * Project: reedsun-homepage
+ * File Created: Sunday, 9th September 2018 7:40:34 pm
+ * Author: ReedSun (sunhongzhao@foxmail.com)
+ * -----
+ * Last Modified: Sunday, 9th September 2018 10:11:11 pm
+ * Modified By: ReedSun (sunhongzhao@foxmail.com>)
+ * -----
+ * Copyright 2018 - 2018, Shanjing
+ */
 // ref: https://umijs.org/config/
 export default {
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: true,
-      title: '111',
-      dll: true,
-      pwa: true,
-      routes: {
-        exclude: [],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: true,
+        title: 'ReedSun',
+        dll: true,
+        pwa: true,
+        routes: {
+          exclude: [],
+        },
+        hardSource: true,
       },
-      hardSource: true,
-    }],
+    ],
   ],
-}
+};
