098bf153ed77e2db5509b8c3b58be318a5e5c502
ReedSun <superreedsun@gmail.com>
2018-09-10 19:20:29 +0800

    complete homepage basic structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,38 @@
  * File Created: Sunday, 9th September 2018 7:40:34 pm
  * Author: ReedSun (sunhongzhao@foxmail.com)
  * -----
- * Last Modified: Sunday, 9th September 2018 10:11:04 pm
+ * Last Modified: Tuesday, 11th September 2018 2:06:25 pm
  * Modified By: ReedSun (sunhongzhao@foxmail.com>)
  * -----
  * Copyright 2018 - 2018, Shanjing
  */
 import React from 'react';
+import styles from './index.less';
 
 export default function() {
-  return <div>首页</div>;
+  return (
+    <div>
+      <div className={styles.fullPage}>
+        <h1>ReedSun</h1>
+      </div>
+      <div className={styles.fullPage}>
+        <h1>Maybe a programmer</h1>
+        <p>
+          <span>GitHub</span>
+        </p>
+      </div>
+      <div className={styles.fullPage}>
+        <h1>Maybe a game player</h1>
+        <p>
+          <span>PlayStation</span>
+          <span>Steam</span>
+          <span>other</span>
+        </p>
+      </div>
+      <div className={styles.fullPage}>
+        <h1>Maybe just an ordinary person</h1>
+        <p>To be added</p>
+      </div>
+    </div>
+  );
 }
