e041f8a28d0249314c17a51f57c25b1add473d88
ReedSun <superreedsun@gmail.com>
2018-09-10 19:59:49 +0800

    add global header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,15 +4,44 @@
  * File Created: Sunday, 9th September 2018 7:40:34 pm
  * Author: ReedSun (sunhongzhao@foxmail.com)
  * -----
- * Last Modified: Sunday, 9th September 2018 10:10:59 pm
+ * Last Modified: Monday, 10th September 2018 7:59:23 pm
  * Modified By: ReedSun (sunhongzhao@foxmail.com>)
  * -----
  * Copyright 2018 - 2018, Shanjing
  */
 import React from 'react';
+import classNames from 'classnames';
+import Link from 'umi/link';
+import styles from './index.less';
 
-function BasicLayout({ children }) {
-  return <div>{children}</div>;
+function BasicLayout(props) {
+  const { children } = props;
+  return (
+    <div>
+      <div className={styles.header}>
+        <div className={styles.logo}>ReedSun</div>
+        <div className={styles.menu}>
+          <Link
+            className={classNames(styles.menuItem, {
+              [styles.actionMenuItem]: location.pathname === '/',
+            })}
+            to="/"
+          >
+            首页
+          </Link>
+          <Link
+            className={classNames(styles.menuItem, {
+              [styles.actionMenuItem]: location.pathname.slice(0, 9) === '/torphies',
+            })}
+            to="/torphies"
+          >
+            成就
+          </Link>
+        </div>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 }
 
 export default BasicLayout;
