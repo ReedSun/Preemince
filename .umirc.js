d0b20532a5b60c10943825ad5fbcba233e259bdc
ReedSun <superreedsun@gmail.com>
2018-09-13 20:01:59 +0800

    umi config add alias '@'

--- .umirc.js
+++ .umirc.js
@@ -4,20 +4,27 @@
  * File Created: Sunday, 9th September 2018 7:40:34 pm
  * Author: ReedSun (sunhongzhao@foxmail.com)
  * -----
- * Last Modified: Sunday, 9th September 2018 10:11:11 pm
+ * Last Modified: Friday, 14th September 2018 2:17:18 pm
  * Modified By: ReedSun (sunhongzhao@foxmail.com>)
  * -----
  * Copyright 2018 - 2018, Shanjing
  */
 // ref: https://umijs.org/config/
+const path = require('path');
+
 export default {
+  alias: {
+    '@': path.resolve(__dirname, 'src/'),
+  },
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     [
       'umi-plugin-react',
       {
         antd: true,
-        dva: true,
+        dva: {
+          immer: true,
+        },
         dynamicImport: true,
         title: 'ReedSun',
         dll: true,
