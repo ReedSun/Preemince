f607757199ad8ebaec3d12031ee2131ce6af1091
ReedSun <superreedsun@gmail.com>
2018-09-13 20:18:11 +0800

    trophies header url use localStorage for cache

--- src/models/trophies.js
+++ src/models/trophies.js
@@ -4,7 +4,7 @@
  * File Created: Friday, 14th September 2018 2:19:32 pm
  * Author: ReedSun (sunhongzhao@foxmail.com)
  * -----
- * Last Modified: Thursday, 13th September 2018 7:59:15 pm
+ * Last Modified: Thursday, 13th September 2018 8:16:14 pm
  * Modified By: ReedSun (sunhongzhao@foxmail.com>)
  * -----
  * Copyright 2018 - 2018, Shanjing
@@ -15,13 +15,16 @@ export default {
   namespace: 'trophies',
 
   state: {
-    pageConfig: {},
+    pageConfig: {
+      header_url: localStorage.getItem('trophies_header_url'),
+    },
   },
 
   effects: {
     *getPageConfig(_, { call, put }) {
       const response = yield call(getTrophiesPageConfig);
       if (response.isRight) {
+        localStorage.setItem('trophies_header_url', response.body.header_url);
         yield put({
           type: 'changePageConfig',
           payload: response.body,
