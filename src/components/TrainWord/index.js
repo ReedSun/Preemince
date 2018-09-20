b307a555825f5d9b8e7ace350b803208ea475e25
ReedSun <superreedsun@gmail.com>
2018-09-20 21:54:06 +0800

    complete TrainWord

--- src/components/TrainWord/index.js
+++ src/components/TrainWord/index.js
@@ -4,22 +4,58 @@
  * File Created: Friday, 20th September 2018 8:43:37 pm
  * Author: ReedSun (sunhongzhao@foxmail.com)
  * -----
- * Last Modified: Thursday, 20th September 2018 8:48:12 pm
+ * Last Modified: Thursday, 20th September 2018 9:51:02 pm
  * Modified By: ReedSun (sunhongzhao@foxmail.com>)
  * -----
  * Copyright 2018 - 2018, Shanjing
  */
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import styles from './index.less';
 
 export default class TarinWord extends PureComponent {
   static propTypes = {
     children: PropTypes.string.isRequired,
+    height: PropTypes.number,
+    fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    trainColor: PropTypes.string,
+    wordColor: PropTypes.string,
+  };
+
+  static defaultProps = {
+    fontSize: 30,
+    height: 64,
+    trainColor: 'indianred',
+    wordColor: 'lightblue',
   };
 
   render() {
-    const { children } = this.props;
+    const { children, fontSize, height, trainColor, wordColor } = this.props;
 
-    return <span>{children}</span>;
+    const wrapperStyles = {
+      fontSize,
+      height,
+    };
+
+    const textStyles = {
+      color: wordColor,
+      lineHeight: `${height}px`,
+    };
+
+    const trainStyles = {
+      color: trainColor,
+      lineHeight: `${height}px`,
+    };
+
+    return (
+      <div className={styles.wrapper} style={wrapperStyles}>
+        <div className={styles.text} style={textStyles}>
+          {children}
+        </div>
+        <div className={styles.trainWrapper} style={trainStyles}>
+          <div className={styles.train}>{children}</div>
+        </div>
+      </div>
+    );
   }
 }
